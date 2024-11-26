06681dae5825ea104458ecf8c3a653c3cee9392c
Omer Talmi <43521606+omertalmi5@users.noreply.github.com>
2024-11-26 18:57:22 +0200

    Encode image

--- losses/clip_loss.py
+++ losses/clip_loss.py
@@ -109,7 +109,7 @@ class CLIPLoss(torch.nn.Module):
 
     def compute_text_direction(self, source_class: str, target_class: str) -> torch.Tensor:
         source_features = self.get_text_features(source_class)
-        target_features = self.get_text_features(target_class)
+        target_features = self.get_image_features(target_class)
 
         text_direction = (target_features - source_features).mean(axis=0, keepdim=True)
         text_direction /= text_direction.norm(dim=-1, keepdim=True)
